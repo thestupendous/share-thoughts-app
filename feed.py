6ba4cab477d094705c44eaca3dcad143f77b4271
new user <abcnotreal@gmail.com>
2019-11-01 01:36:39 +0530

    modified:   basic-idea.txt - added feeds database 	modified:   feed.py - added functionality without db connection 	modified:   templates/feed.html  - basic structure for feeds,comments using table 	new file:   templates/table_code.html - temporary file

--- feed.py
+++ feed.py
@@ -1,23 +1,30 @@
 import os
 #from pymongo import MongoClient
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 #mongo_host = os.environ.get('MONGOHOST','localhost')
 #client = MongoClient(mongo_host,'27017')
 
 app = Flask(__name__)
+@app.route('/')
+def delete_route():
+    return redirect('/feed')
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/feed/post',methods=['GET','POST'])
+def post_status():
+    uid = request.form['uid']
+    status = request.form['status']
+
+    print('\n\n','uid and status ',uid,status,sep='  ',end='\n\n')
+    return redirect('/feed')
+
+    # -- make db call to update posts collection to add new entry for 
+    #    corresponding uid and suitable post
+
+@app.route('/feed',methods=['GET','POST'])
 def base():
-    # call auth()
-
-    # if not auth() return error, load again
-
-    # if auth() ,
-    #   call feed()
-    print('*'*30,'printing req \n',request)
-    print('\n','printing \n',request.form,'\n\n\n')
-    words = request.args['texts']
-    return render_template('index2.html',word=words)
+    # expecting userid as post request
+    uid = '1'
+    return render_template('feed.html',uid=uid)
 
 if __name__ == '__main__':
     app.run(debug=True,port='5020')
